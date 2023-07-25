a6534a73770d222b37dab8dc6d8930b20cde4c79
diahrita <92697475+diahrita@users.noreply.github.com>
2023-07-25 10:03:29 +0700

    project4

--- project4/index.php
+++ project4/index.php
@@ -8,16 +8,23 @@
 </head>
 
 <body>
-
   <h3>Pendaftaran Siswa Baru</h3>
   <h1>Digital Talent</h1>
-
-  <h4>Menu :</h4>
+  <h3>Menu</h3>
   <ul>
     <li><a href="form_daftar.php">Daftar Baru</a></li>
     <li><a href="list_siswa.php">Pendaftar</a></li>
   </ul>
-
+  <br>
+  <?php
+  if (isset($_GET['status'])) {
+    if ($_GET['status'] == "sukses") {
+      echo "Pendaftaran siswa baru berhasil!";
+    } elseif ($_GET['status'] == "gagal") {
+      echo "Pendaftaran siswa baru gagal!";
+    }
+  }
+  ?>
 </body>
 
 </html>
